9885856d4869f24b35788fe1ba4ebd777c67ce51
Adco <adco>
2023-01-07 12:44:08 -0500

    Initial Commit

---
+++ OCR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR
+//
+//  Created by Andres Cortes on 1/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
